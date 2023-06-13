a7b661536f4d0b6cdeefd521f3572b1e34192992
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2023-06-13 16:55:21 -0300

    Alterações na Home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <Router>
-      <ul>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contatos</Link>
-          <Link to="/company">Empresa</Link>
-          <Link to="/newproject">Novos Projetos</Link>
-        
-      </ul>
-
+      <NavBar/>
+      
       <Routes>
         <Route path="/" element={<Container customClass = "min-height"><Home /></Container>} />
+        <Route path="/projects" element={<Container><Projects /></Container>} />
         <Route path="/company" element={<Container><Company /></Container>} />
         <Route path="/contact" element={<Container><Contact /></Container>} />
         <Route path="/newproject" element={<Container><NewProject /></Container>} />
+
       </Routes>
+      <Footer />  
     </Router>
+    
   );
 }
 
